758fb9e0c9ee61101341e2384d69c133f0a4c271
Berk Utku Yenisey <berkutkuyenisey@gmail.com>
2018-02-16 21:36:16 +0300

    Fixed More Errors

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -42,61 +42,88 @@ public class AppTest
     }
    
 	public void testFor4() {
-		Integer a[] = {1,2,3};
-		Integer b[] = {1,2,3,4};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
 		App test = new App();
 		int output = test.howManyCD(a, b, 1, 2);
 		assertEquals(output,4);
 	}
 	
-	
 	public void testFor3() {
-		Integer a[] = {1,2,3};
-		Integer b[] = {1,2,3,4};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
+		b.add(4);
 		App test = new App();
 		int output = test.howManyCD(a, b, 1, 4);
 		assertEquals(output,3);
 	}
 	
-	
 	public void testFor2() {
-		Integer a[] = {1,2,3};
-		Integer b[] = {1,2,3,4};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
+		b.add(4);
 		App test = new App();
 		int output = test.howManyCD(a, b, 1, 5);
 		assertEquals(output,2);
 	}
 	
-	
 	public void testFor1() {
-		Integer a[] = {1,2,3};
-		Integer b[] = {1,2,3,4};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
+		b.add(4);
 		App test = new App();
 		int output = test.howManyCD(a, b, 4, 5);
 		assertEquals(output,1);
 	}
 	
-
 	public void testFor0() {
-		Integer a[] = {};
-		Integer b[] = {};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> b = new ArrayList<Integer>();
 		App test = new App();
 		int output = test.howManyCD(a, b, 6, 5);
 		assertEquals(output,0);
 	}
 	
-	
 	public void testForNullB() {
 		App test = new App();
-		Integer b[] = {1,2,3};
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
 		int output = test.howManyCD(null, b, 3, 1);
 		assertEquals(output,-1);
 	}
 	
-	
 	public void testForNullA() {
 		App test = new App();
-		Integer a[] = {1,2,3};
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
 		int output = test.howManyCD(a, null, 3, 1);
 		assertEquals(output,-1);
 	}
